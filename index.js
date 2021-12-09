38c5392d082e74b5e317f3557b92016304e9b488
thomas_villard <tvillard@oui.sncf>
2021-12-09 10:05:16 +0100

    vers 1

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-module.exports = function areEqualTo(a, b) {
-    if (a === b) return true;
-    return false;
+module.exports = {
+    randomInArray = (tbl) => {
+        return tbl[Math.floor(Math.random() * tbl.length)];
+    },
+    firstLetterInUpperCase = (strl) => {
+        let tbl = str.split("");
+        let up = tbl[0].toUpperCase();
+        tbl.shift();
+        tbl.unshift(up)
+        let final = tbl.join("");
+        return final;
+    },
+    randomNumber = (min, max)  => {
+        return min + Math.floor(Math.random() * (max - min));
+    },
+    getLocalTime = ()  => {
+        return new Date().toLocaleTimeString();
+    },
+    reverseString = (str) => {
+        return str.split("").reverse().join("");
+    }
 }

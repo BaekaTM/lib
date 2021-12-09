0aca5b3aefdc009b4297225bfafb8eb51f1d05a5
thomas_villard <tvillard@oui.sncf>
2021-12-09 10:26:47 +0100

    test

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
+var randomInArray = (tbl) => {
+
+    return tbl[Math.floor(Math.random() * tbl.length)];
+
+}
+
+var  firstLetterInUpperCase = (strl) => {
+
+    let tbl = str.split("");
+    let up = tbl[0].toUpperCase();
+    tbl.shift();
+    tbl.unshift(up)
+    let final = tbl.join("");
+    return final;
+
+}
+
+var randomNumber = (min, max)  => {
+
+    return min + Math.floor(Math.random() * (max - min));
+
+}
+
+var getLocalTime = ()  => {
+
+    return new Date().toLocaleTimeString();
+
+}
+
+var reverseString = (str) => {
+
+    return str.split("").reverse().join("");
+
+}
+
 module.exports = {
-    randomInArray = (tbl) => {
-        return tbl[Math.floor(Math.random() * tbl.length)];
-    },
-    firstLetterInUpperCase = (strl) => {
-        let tbl = str.split("");
-        let up = tbl[0].toUpperCase();
-        tbl.shift();
-        tbl.unshift(up)
-        let final = tbl.join("");
-        return final;
-    },
-    randomNumber = (min, max)  => {
-        return min + Math.floor(Math.random() * (max - min));
-    },
-    getLocalTime = ()  => {
-        return new Date().toLocaleTimeString();
-    },
-    reverseString = (str) => {
-        return str.split("").reverse().join("");
-    }
+
+    randomInArray,
+    firstLetterInUpperCase,
+    randomNumber,
+    getLocalTime,
+    reverseString
+
 }

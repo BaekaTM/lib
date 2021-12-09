5a0ec1e0aa16901f873d037ddd79edc94ca72ee1
thomas_villard <tvillard@oui.sncf>
2021-12-09 11:23:27 +0100

    added all

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 lib = require('@baeka/private-tools')
 
-function test(array) {
+function test() {
 
     var  test_values = [["a", "b", "c", "d"], "the t should be in upper case", [400, 600], , "desrever eb tn'dluohs ti"]
     var i = 0;
